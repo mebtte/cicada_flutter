f6bdcdcc16ffadfcbda6c6f8848ad43d8513ee53
mebtte <i@mebtte.com>
2025-08-12 23:03:22 +0800

    audio handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'package:audio_service/audio_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:provider/provider.dart';
 import './app.dart';
 import './states/server.dart';
+import './audio_handler.dart';
+
+final getIt = GetIt.instance;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final serverState = ServerState();
   await serverState.initialize();
+
+  var audioHandler = await AudioService.init(builder: () => MyAudioHandler());
+  getIt.registerSingleton(audioHandler);
+
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider.value(value: serverState)],
--- lib/pages/server_management/index.dart
+++ lib/pages/server_management/index.dart
@@ -1,4 +1,6 @@
+import 'package:cicada/audio_handler.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class PageServerManagement extends StatelessWidget {
   const PageServerManagement({super.key});
@@ -7,7 +9,27 @@ class PageServerManagement extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Server Management')),
-      body: Center(child: Text('server')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            var audioHandler = GetIt.instance<MyAudioHandler>();
+            // var item = MediaItem(
+            //   id: 'https://music.mebtte.com/asset/music/c35d5d229458cd5266e366e4a5bee717.m4a',
+            //   album: 'Album name',
+            //   title: 'Track title',
+            //   artist: 'Artist name',
+            //   duration: const Duration(milliseconds: 123456),
+            //   artUri: Uri.parse(
+            //     'https://music.mebtte.com/asset/music_cover/8de82c491047b3bddbd88f8b21408e72.jpg',
+            //   ),
+            // );
+            // await audioHandler.playMediaItem(item);
+            // await audioHandler.play();
+            audioHandler.playTest();
+          },
+          child: Text("play123"),
+        ),
+      ),
     );
   }
 }
